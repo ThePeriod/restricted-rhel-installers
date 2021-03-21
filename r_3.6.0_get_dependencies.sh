68bdc29b60600d7144a9028d8190709c0e6ac7f4
Dante Chavez <dchaveza@utec.edu.pe>
2021-03-21 18:52:14 -0500

    wget on full list of dependencies

--- r_3.6.0_get_dependencies.sh
+++ r_3.6.0_get_dependencies.sh
@@ -1,12 +1,24 @@
 R_VERSION=3.6.0
 wget https://cdn.rstudio.com/r/centos-7/pkgs/R-${R_VERSION}-1-1.x86_64.rpm
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/bzip2-devel-1.0.6-13.el7.x86_64.rpm
+wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/gcc-4.8.5-28.el7_5.1.x86_64.rpm --no-check-certificate
 wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/gcc-c++-4.8.5-28.el7_5.1.x86_64.rpm --no-check-certificate
 wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/gcc-gfortran-4.8.5-28.el7_5.1.x86_64.rpm --no-check-certificate
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/libcurl-devel-7.29.0-59.el7.x86_64.rpm
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/libicu-devel-50.2-4.el7_7.x86_64.rpm
+wget http://mirror.centos.org/centos/7/os/x86_64/Packages/libSM-1.2.2-2.el7.x86_64.rpm
+wget http://vault.centos.org/centos/7.7.1908/os/Source/SPackages/libtiff-4.0.3-27.el7_3.src.rpm
+wget http://mirror.centos.org/centos/7/os/x86_64/Packages/libXmu-1.1.2-2.el7.x86_64.rpm
+wget http://mirror.centos.org/centos/7/os/x86_64/Packages/libXt-1.1.5-3.el7.x86_64.rpm
+wget http://52.33.4.66/hdp_utils/centos/7/openblas/openblas-devel-0.2.19-4.el7.x86_64.rpm
+wget https://koji.xcp-ng.org/kojifiles/packages/pango/1.40.4/1.el7/x86_64/pango-1.40.4-1.el7.x86_64.rpm --no-check-certificate
+wget http://mirror.centos.org/centos/7/os/x86_64/Packages/pcre-devel-8.32-17.el7.x86_64.rpm
+wget http://mirror.centos.org/centos/7/os/x86_64/Packages/tcl-8.5.13-8.el7.x86_64.rpm
+wget http://mirror.centos.org/centos/7/os/x86_64/Packages/tk-8.5.13-6.el7.x86_64.rpm
+wget https://koji.xcp-ng.org/kojifiles/packages/unzip/6.0/19.el7/x86_64/unzip-6.0-19.el7.x86_64.rpm --no-check-certificate
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/xz-devel-5.2.2-1.el7.x86_64.rpm
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/zip-3.0-11.el7.x86_64.rpm
+wget https://koji.xcp-ng.org/kojifiles/packages/zlib/1.2.7/17.el7/x86_64/zlib-devel-1.2.7-17.el7.x86_64.rpm --no-check-certificate
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/gcc-4.8.5-44.el7.x86_64.rpm
 wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/libstdc++-4.8.5-28.el7_5.1.x86_64.rpm --no-check-certificate
 wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/libstdc++-devel-4.8.5-28.el7_5.1.x86_64.rpm --no-check-certificate
