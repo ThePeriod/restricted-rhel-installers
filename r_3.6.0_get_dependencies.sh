ac0934c521360b67f5b00cdf5172daa18a9a6536
Dante Chavez <dchaveza@utec.edu.pe>
2021-03-21 15:52:23 -0500

    Added --no-check-certificate

--- r_3.6.0_get_dependencies.sh
+++ r_3.6.0_get_dependencies.sh
@@ -1,15 +1,15 @@
 R_VERSION=3.6.0
 wget https://cdn.rstudio.com/r/centos-7/pkgs/R-${R_VERSION}-1-1.x86_64.rpm
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/bzip2-devel-1.0.6-13.el7.x86_64.rpm
-wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/gcc-c++-4.8.5-28.el7_5.1.x86_64.rpm
-wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/gcc-gfortran-4.8.5-28.el7_5.1.x86_64.rpm
+wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/gcc-c++-4.8.5-28.el7_5.1.x86_64.rpm --no-check-certificate
+wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/gcc-gfortran-4.8.5-28.el7_5.1.x86_64.rpm --no-check-certificate
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/libcurl-devel-7.29.0-59.el7.x86_64.rpm
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/libicu-devel-50.2-4.el7_7.x86_64.rpm
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/xz-devel-5.2.2-1.el7.x86_64.rpm
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/zip-3.0-11.el7.x86_64.rpm
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/gcc-4.8.5-44.el7.x86_64.rpm
-wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/libstdc++-4.8.5-28.el7_5.1.x86_64.rpm
-wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/libstdc++-devel-4.8.5-28.el7_5.1.x86_64.rpm
-wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/libquadmath-devel-4.8.5-28.el7_5.1.x86_64.rpm
+wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/libstdc++-4.8.5-28.el7_5.1.x86_64.rpm --no-check-certificate
+wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/libstdc++-devel-4.8.5-28.el7_5.1.x86_64.rpm --no-check-certificate
+wget https://koji.xcp-ng.org/kojifiles/packages/gcc/4.8.5/28.el7_5.1/x86_64/libquadmath-devel-4.8.5-28.el7_5.1.x86_64.rpm --no-check-certificate
 wget http://ftp.mirrorservice.org/sites/ftp.scientificlinux.org/linux/scientific/7.2/x86_64/updates/security/libcurl-7.29.0-42.el7.x86_64.rpm
 wget http://mirror.centos.org/centos/7/os/x86_64/Packages/libicu-50.2-4.el7_7.x86_64.rpm
